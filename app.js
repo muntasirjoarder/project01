2cd4dd4b8c6ae2b514f706c06c4697743e79b548
Muntasir Joarder <muntasir.joarder@gmail.com>
2017-10-05 16:05:04 +1000

    more parts

--- app.js
+++ app.js
@@ -3,3 +3,28 @@ const restify = require('restify');
 const mdb = require('moviedb')(process.env.MOVIE_DB_API_KEY);
 const wordsToNum = require('words-to-num');
 require('datejs');
+
+// Setup Restify Server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+const connector = new builder.ChatConnector({
+  appId: process.env.MICROSOFT_APP_ID,
+  appPassword: process.env.MICROSOFT_APP_PASSWORD,
+});
+const bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+const luisUrl = 'api.projectoxford.ai/luis/v1';
+const luisModelUrl = `https://${luisUrl}/application?id=${process.env.LUIS_APP_ID}&subscription-key=${process.env.LUIS_API_KEY}`;
+
+const recognizer = new builder.LuisRecognizer(luisModelUrl);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', intents);
+
+// Get this information with mdb.configuration()
+const imagesBaseUrl = 'https://image.tmdb.org/t/p/';
+const posterSize = 'w185';
